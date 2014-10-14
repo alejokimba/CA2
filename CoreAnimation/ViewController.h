a388c9dfb40a3459f906c21836ea07d4f8623bbf
Alumno <alumno@Multimedias-Mac-Pro-9.local>
2014-10-14 09:38:15 -0500

    Initial Commit

---
+++ CoreAnimation/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CoreAnimation
+//
+//  Created by Alumno on 14/10/14.
+//  Copyright (c) 2014 Jav1792. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ CoreAnimation/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CoreAnimation
+//
+//  Created by Alumno on 14/10/14.
+//  Copyright (c) 2014 Jav1792. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
